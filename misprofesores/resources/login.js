20b6160993a0dc68aae50c1c82155f0eb7d1ef94
isaac <isaaccabrera31@gmail.com>
2019-11-18 13:46:07 -0600

    Login mock implementation

---
+++ misprofesores/resources/login.js
@@ -0,0 +1,46 @@
+"use strict"
+/**
+ * Login
+ */
+let loginForm = document.querySelector("form");
+let inputEmail = loginForm.querySelectorAll("input")[0];
+let inputPass = loginForm.querySelectorAll("input")[1];
+let loginBtn = loginForm.querySelector("button");
+
+loginForm.addEventListener("keyup", (e) => enableLoginBtn(e));
+loginBtn.addEventListener("click", (e) => login(e));
+
+function enableLoginBtn() {
+    loginBtn.disabled = inputEmail.value.length < 0 || !inputEmail.validity.valid 
+                        || inputPass.value == 0;
+}
+
+function login(event) {
+    console.log("Login");
+    //
+    let xhr = new XMLHttpRequest();
+    /** The endpoint will change to /${email} when we have our backend,
+     * or we could user another endpoint /login and send the email and pass.
+     * Json-server doesn´t allow us to add more than 5 fields in the db,
+     * that's why we don´t use it.
+    */
+    xhr.open("GET", `https://my-json-server.typicode.com/Isaac2357/MisProfesoresServer/usuarios/${1}`);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send();
+    xhr.onload = function() {
+        console.log(xhr.status, xhr.statusText);
+        if (xhr.status == 200) {
+            let response = JSON.parse(xhr.response);
+            console.log(response);
+            if (response.correo == inputEmail.value && response["contraseña"] == inputPass.value) {
+                window.open('PantallaPrincipal.html', '_self',false)
+                localStorage.token = "asdfgh123"
+                localStorage.usermail  = inputEmail.value;
+            } else {
+                alert('Usuario no registrado.')
+            }
+        } else {
+            alert('Ocurrió un error.')
+        }
+    }
+}
