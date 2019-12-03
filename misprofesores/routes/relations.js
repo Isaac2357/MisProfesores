b0fe4b6b10d1e3d86c90732f6684b3f96bb3e339
manuelsanchez <manuel.vma12@gmail.com>
2019-12-03 17:14:19 -0600

    listo

--- misprofesores/routes/relations.js
+++ misprofesores/routes/relations.js
@@ -1,16 +1,90 @@
 const express = require('express');
 const router = express.Router();
 const Relation = require('../models/Relation')
+const User = require('../models/User')
+const types = ["ADMIN", "PROF", "ALUMN"];
+
 const {auth} = require("../middlewares/auth")
 
 
 router.route('/')
 .get(auth, async (req, res) => {
-    res.send("relations get");
+    try {
+        let usrs = await Relation.getRelations();
+        if (usrs != null) {
+            let mappedUsrs = usrs.map((user) => {
+                let usr = {};
+                const fields = ["rid",
+                "idProfesor",
+                "idCurso",
+                "periodo",
+                "year"];
+                                
+                for (let key in user) {
+                    if (fields.includes(key)) {
+                        usr[key] = user[key];
+                    }
+                }
+                return usr;
+            });
+            res.send(mappedUsrs);
+        } else {
+            res.status(400);
+            res.statusMessage = "Internal error."
+            res.send();
+        }
+    } catch (error) {
+        res.status(400);
+        res.statusMessage = "Internal error."
+        res.send();
+    }
 })
 .post(auth, async (req, res) => {
-    res.send("relations post");
+    let type = req.tipo;
+    if (type != undefined && type == "ADMIN") {
+        let data = req.body;
+        if (data != null && !isEmpty(data)) {
+            if (!isValidUser(data)) {
+                res.status(406);
+                res.statusMessage = "Invalid payload."
+                res.send();
+            } else {
+                try {
+                    let usr = await Relation.getRelation(data.idProfesor);
+                    if (usr != null) {
+                        res.status(406);
+                        res.statusMessage = `User with id: ${data.idProfesor} already exists in the database.`
+                        res.send();
+                    } {
+
+                            let doc = await Relation.createRelation(data);
+                            if (doc) {
+                                res.status(201);
+                                res.statusMessage = "Relacion Creada"
+                                res.send();
+                            } else {
+                                res.statusCode = 400;
+                                res.send();
+                            }
+                            
+                        }
+                    }
+                 catch (error) {
+                    console.log(error);
+                    res.status(500);
+                    res.statusMessage = "Internal server error";
+                    res.send();
+                }
+            }   
+        } else {
+            res.status(406);
+            res.statusMessage = "Empty payload."
+            res.send();
+        }
+
+    } 
 })
+
 .delete(auth, async (req, res) => {
     res.send("relations delete");
 })
@@ -18,4 +92,40 @@ router.route('/')
     res.send("relations put");
 });
 
+function formatRelation(relacion) {
+    let c = {};
+    const fields = ["rid",
+                    "idProfesor",
+                    "idCurso",
+                    "periodo",
+                    "year"];
+                    
+    for (let key in relacion) {
+        if (fields.includes(key)) {
+            c[key] = relacion[key];
+        }
+    }
+    return c;
+}
+function isValidUser(user) {
+    if (user != null) {
+        let validFields = 0;
+        for (let key in user) {
+            if (!fields.includes(key)) {
+                return false;
+            } else {
+                validFields++;
+            }
+        }
+        return validFields == fields.length
+    }
+    return false;
+}
+function isEmpty(payload) {
+    for(let key in payload) {
+        if(payload.hasOwnProperty(key))
+            return false;
+    }
+    return true;  
+}
 module.exports = router;
